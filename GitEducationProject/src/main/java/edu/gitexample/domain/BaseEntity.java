4cf6ddb360c79ad906cd064f837143ec610e7b78
Andriy Zhmurkevych <gmand@ukr.net>
2018-05-15 11:15:11 +0300

    MASTER: added BaseEntity class

---
+++ GitEducationProject/src/main/java/edu/gitexample/domain/BaseEntity.java
@@ -0,0 +1,14 @@
+package edu.gitexample.domain;
+
+public abstract class BaseEntity {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
